6bc24e7d213f7fc551461347fa20f49f455265f5
annazus <suzanna@appliedar.us>
2019-04-10 08:35:48 -0700

    Completed increment functionality

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
   render() {
-    return <h1>Click Count with Enzyme</h1>;
+    return (
+      <div data-test="component-app">
+        <p data-test="component-app-display">Counter = {this.state.count}</p>
+        <button
+          data-test="component-app-button"
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          +1
+        </button>
+      </div>
+    );
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,41 @@
-import React from "react";
-import Enzyme, { shallow } from "enzyme";
+import Enzyme from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
+import { setUp, fetchComponent } from "./utils/test/testUtils";
 import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
 test("renders without crashing", () => {
-  shallow(<App />);
+  const wrapper = setUp(App);
+});
+
+test("renders the App component", () => {
+  const wrapper = setUp(App);
+  const appComponent = fetchComponent(wrapper, "component-app");
+  expect(appComponent.length).toBe(1);
+});
+
+test("renders a counter display", () => {
+  const wrapper = setUp(App);
+  const displayComponent = fetchComponent(wrapper, "component-app-display");
+  expect(displayComponent.length).toBe(1);
+});
+test("renders a button ", () => {
+  const wrapper = setUp(App);
+  const button = fetchComponent(wrapper, "component-app-button");
+  expect(button.length).toBe(1);
+});
+
+test("counter starts at 0", () => {
+  const wrapper = setUp(App);
+  expect(wrapper.state().count).toBe(0);
+});
+
+test("on clicking button, count display increments", () => {
+  const wrapper = setUp(App, { count: 7 });
+  const button = fetchComponent(wrapper, "component-app-button");
+  button.simulate("click");
+  const displayComponent = fetchComponent(wrapper, "component-app-display");
+  console.log(displayComponent.debug());
+  expect(displayComponent.text()).toContain("8");
 });
